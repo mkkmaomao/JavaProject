e625cd2677d64aa717e9909fc8bc8ebd9b8035d6
xiaoxuema3 <xiaoxuema3-c@my.cityu.edu.hk>
2021-09-07 11:23:23 +0800

    second version

--- JavaProject/src/testPackage/testClass.java
+++ JavaProject/src/testPackage/testClass.java
@@ -1,5 +1,34 @@
 package testPackage;
 
-public class testClass {
+	public class testClass {
+		public boolean isFullHouse(String cards[], int n) {
+			return isThreeOfaKind(cards, n) && isTwoPairs(cards, n);
+		}
+		public boolean isThreeOfaKind(String cards[], int n) {
+			for (int i=0; i<n-2; i++) {
+				if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
+						cards[i+1].charAt(1) == cards[i+2].charAt(1))
+						return true;
+			}
+			return false;
+		}
 
+		// Precondition: A hand of n cards have been sorted by card number.
+		public boolean isTwoPairs(String cards[], int n) {
+			int count = 0;
+			for (int i=0; i<n-1; i++) {
+				if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+					count++;
+					i++;
+				}
+			}
+			if (count == 2)
+				return true;
+			else
+				return false;
+		}
+		public static void main(String args[]) {
+			System.out.println(new testClass().isFullHouse(
+				new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+		}
 }
